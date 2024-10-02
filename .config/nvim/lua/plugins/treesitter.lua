0c398256a592b6e835bd43953f026d7c5efef84a
Finn Willard <finn.willard@gmail.com>
2024-10-02 16:56:57 -0700

    basic nvim setup

---
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,28 @@
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    local treesitter = require("nvim-treesitter.configs")
+    treesitter.setup({
+    -- enable syntax highlighting
+      highlight = { enable = true },
+      -- enable indentation
+      indent = { enable = true },
+
+      -- ensure these language parsers are installed
+      ensure_installed = {
+        "json",
+        "javascript",
+        "typescript",
+        "tsx",
+        "html",
+        "css",
+        "lua",
+        "vim",
+        "gitignore",
+        "c",
+        "rust",
+	},
+      })
+  end
+}
