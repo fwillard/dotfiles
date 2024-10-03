0a9497670e1fa3949c1800480450ff845f32781d
Finn Willard <finn.willard@gmail.com>
2024-10-02 18:00:47 -0700

    added lualine and catppucin integrations

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,13 @@ return {  "catppuccin/nvim",
   name = "catppuccin", 
   priority = 1000,
   config = function()
+    require("catppuccin").setup({
+      integrations = {
+        gitsigns = true,
+        neotree = true,
+        treesitter = true,
+      }
+    })
     vim.cmd.colorscheme "catppuccin"
   end
 }
--- .config/nvim/lua/plugins/neo-tree.lua
+++ .config/nvim/lua/plugins/neo-tree.lua
@@ -5,5 +5,23 @@ return {
     "nvim-lua/plenary.nvim",
     "nvim-tree/nvim-web-devicons", 
     "MunifTanjim/nui.nvim",
-  }
+  },
+  opts = {
+    enable_git_status = true,
+    git_status = {
+      symbols = {
+        -- Change type
+        added     = "", -- or "✚", but this is redundant info if you use git_status_colors on the name
+        modified  = "", -- or "", but this is redundant info if you use git_status_colors on the name
+        deleted   = "✖",-- this can only be used in the git_status source
+        renamed   = "󰁕",-- this can only be used in the git_status source
+        -- Status type
+        untracked = "",
+        ignored   = "",
+        unstaged  = "󰄱",
+        staged    = "",
+        conflict  = "",
+      }
+    },
+  },
 }
