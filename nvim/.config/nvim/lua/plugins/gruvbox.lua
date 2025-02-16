c50ea0a51cddb1bba89041c0a91fa97dd5bf2df9
Finn Willard <finn.willard@gmail.com>
2025-02-16 10:37:05 -0800

    formatting and non-ls setup

--- nvim/.config/nvim/lua/plugins/gruvbox.lua
+++ nvim/.config/nvim/lua/plugins/gruvbox.lua
@@ -1,8 +1,8 @@
-return { 
-  "ellisonleao/gruvbox.nvim", 
-  priority = 1000,  
+return {
+  "ellisonleao/gruvbox.nvim",
+  priority = 1000,
   config = function()
-    require("gruvbox").setup({transparent_mode = true})
+    require("gruvbox").setup({ transparent_mode = true })
     vim.o.background = "dark" -- or "light" for light mode
     vim.cmd([[colorscheme gruvbox]])
   end,
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
-  "nvim-treesitter/nvim-treesitter", 
+  "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
     local config = require("nvim-treesitter.configs")
     config.setup({
-      ensure_installed = {"lua", "python", "c", "rust"},
+      ensure_installed = { "lua", "python", "c", "rust" },
       highlight = { enable = true },
       indent = { enable = true },
     })
-  end
+  end,
 }
